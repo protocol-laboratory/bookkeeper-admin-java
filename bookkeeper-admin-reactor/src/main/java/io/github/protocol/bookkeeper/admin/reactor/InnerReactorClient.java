ac584f71ef6297e6af1c0235c1ba824bb65420b6
ZhangJian He <shoothzj@gmail.com>
2024-08-30 11:18:06 +0800

    feat: init reactor client
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

--- bookkeeper-admin-reactor/src/main/java/io/github/protocol/bookkeeper/admin/reactor/InnerReactorClient.java
+++ bookkeeper-admin-reactor/src/main/java/io/github/protocol/bookkeeper/admin/reactor/InnerReactorClient.java
@@ -1,4 +1,21 @@
 package io.github.protocol.bookkeeper.admin.reactor;
 
+import reactor.netty.http.client.HttpClient;
+
 public class InnerReactorClient {
+    private final HttpClient httpClient;
+
+    private final String httpPrefix;
+
+    public InnerReactorClient(Configuration conf) {
+        HttpClient client = HttpClient.create();
+
+        if (conf.isTlsEnable()) {
+            this.httpPrefix = "https://" + conf.getHost() + ":" + conf.getPort();
+        } else {
+            this.httpPrefix = "http://" + conf.getHost() + ":" + conf.getPort();
+        }
+
+        this.httpClient = client;
+    }
 }
