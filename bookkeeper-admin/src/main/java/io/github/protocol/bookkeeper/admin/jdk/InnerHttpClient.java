5e055c8ffd2e530186d175bcc69bab7a38881537
CennyMo <39120215+CennyMo@users.noreply.github.com>
2025-01-12 22:52:11 +0800

    refactor: refactor http request code by http-facade (#19)
    Signed-off-by: moxiaoying <1159230165@qq.com>

--- bookkeeper-admin/src/main/java/io/github/protocol/bookkeeper/admin/jdk/InnerHttpClient.java
+++ bookkeeper-admin/src/main/java/io/github/protocol/bookkeeper/admin/jdk/InnerHttpClient.java
@@ -1,115 +1,94 @@
 package io.github.protocol.bookkeeper.admin.jdk;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.github.openfacade.http.HttpClient;
+import io.github.openfacade.http.HttpClientConfig;
+import io.github.openfacade.http.HttpClientEngine;
+import io.github.openfacade.http.HttpClientFactory;
+import io.github.openfacade.http.HttpMethod;
+import io.github.openfacade.http.HttpRequest;
+import io.github.openfacade.http.HttpResponse;
+import io.github.openfacade.http.HttpSchema;
+import io.github.openfacade.http.TlsConfig;
+import io.github.openfacade.http.UrlBuilder;
 import io.github.protocol.bookkeeper.admin.api.Configuration;
 import io.github.protocol.bookkeeper.admin.common.JacksonService;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URLEncoder;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class InnerHttpClient {
-    private final Configuration conf;
-
     private final HttpClient client;
 
-    private final String httpPrefix;
+    private UrlBuilder templateUrlBuilder;
 
     public InnerHttpClient(Configuration conf) {
-        this.conf = conf;
-        this.client = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_1_1)
-                .build();
-        this.httpPrefix = "http://" + conf.host + ":" + conf.port;
+        HttpClientConfig.Builder clientConfigBuilder = new HttpClientConfig.Builder();
+        clientConfigBuilder.engine(HttpClientEngine.JAVA);
+        if (conf.tlsEnabled) {
+            TlsConfig.Builder tlsConfigBuilder = new TlsConfig.Builder();
+            io.github.protocol.bookkeeper.admin.api.TlsConfig tlsConfig = conf.tlsConfig;
+            tlsConfigBuilder.cipherSuites(tlsConfig.cipherSuites);
+            tlsConfigBuilder.hostnameVerifyDisabled(tlsConfig.hostnameVerifyDisabled);
+            tlsConfigBuilder.keyStore(tlsConfig.keyStorePath, tlsConfig.keyStorePassword);
+            tlsConfigBuilder.trustStore(tlsConfig.trustStorePath, tlsConfig.trustStorePassword);
+            tlsConfigBuilder.verifyDisabled(tlsConfig.verifyDisabled);
+            clientConfigBuilder.tlsConfig(tlsConfigBuilder.build());
+        }
+        this.client = HttpClientFactory.createHttpClient(clientConfigBuilder.build());
+        templateUrlBuilder = new UrlBuilder();
+        templateUrlBuilder.setHttpSchema(conf.tlsEnabled ? HttpSchema.HTTPS : HttpSchema.HTTP).setHost(conf.host)
+                          .setPort(conf.port);
     }
 
-    public HttpResponse<String> get(String urlSuffix) throws Exception {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(this.httpPrefix + urlSuffix))
-                .GET()
-                .build();
-        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    public HttpResponse put(String url) throws IOException, InterruptedException, ExecutionException {
+        return this.innerPut(url, new byte[0]);
     }
 
-    public HttpResponse<String> post(String url, Object body, String... params)
-            throws IOException, InterruptedException {
-        return this.post(url, objectToString(body), params);
+    public HttpResponse put(String url, Object body) throws IOException, InterruptedException, ExecutionException {
+        return this.innerPut(url, objectToBytes(body));
     }
 
-    public HttpResponse<String> post(String url, String body, String... params)
-            throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(getUri(url, params))
-                .POST(HttpRequest.BodyPublishers.ofString(body))
-                .build();
-        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    private HttpResponse innerPut(String url, byte[] body, String... params)
+        throws InterruptedException, ExecutionException {
+        Map<String, List<String>> headers = new HashMap<>();
+        headers.put("Content-Type", Arrays.asList("application/json"));
+        HttpRequest
+            request = new HttpRequest(concatUrlWithParams(url, params), HttpMethod.PUT, headers, body);
+        return client.send(request).get();
     }
 
-    public HttpResponse<String> post(String url) throws IOException, InterruptedException {
-        return this.post(url, "");
+    public HttpResponse get(String url, String... params) throws IOException, InterruptedException, ExecutionException {
+        HttpRequest request = new HttpRequest(concatUrlWithParams(url, params), HttpMethod.GET);
+        return client.send(request).get();
     }
 
-    public HttpResponse<String> put(String url) throws IOException, InterruptedException {
-        return this.put(url, "");
-    }
-
-    public HttpResponse<String> put(String url, Object body, String... params)
-            throws IOException, InterruptedException {
-        return this.put(url, objectToString(body), params);
-    }
-
-    public HttpResponse<String> put(String url, String body, String... params)
-            throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(getUri(url, params))
-                .PUT(HttpRequest.BodyPublishers.ofString(body))
-                .build();
-        return client.send(request, HttpResponse.BodyHandlers.ofString());
-    }
-
-    public HttpResponse<String> delete(String url, String... requestParams)
-            throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(getUri(url, requestParams))
-                .DELETE()
-                .build();
-        return client.send(request, HttpResponse.BodyHandlers.ofString());
-    }
-
-    private URI getUri(String url, String... params) {
-        return URI.create(this.httpPrefix + url + mapToParams(params));
-    }
-
-    static String mapToParams(String... requestParams) {
+    private List<UrlBuilder.Param> convertListToParams(String... requestParams) {
         if (requestParams.length % 2 != 0) {
             throw new IllegalArgumentException("params list length cannot be odd");
         }
-        if (requestParams.length == 0) {
-            return "";
+        List<UrlBuilder.Param> queryParams = new ArrayList<>();
+        for (int i = 0; i < requestParams.length; i = i + 2) {
+            queryParams.add(new UrlBuilder.Param(requestParams[i], requestParams[i + 1]));
         }
-        StringBuilder res = new StringBuilder("?");
-        res.append(requestParams[0]);
-        res.append('=');
-        res.append(requestParams[1]);
-        for (int i = 2; i < requestParams.length; ) {
-            res.append('&');
-            res.append(encode(requestParams[i++]));
-            res.append('=');
-            res.append(encode(requestParams[i++]));
+        return queryParams;
+    }
+
+    private byte[] objectToBytes(Object obj) throws JsonProcessingException {
+        return obj == null ? new byte[0] : JacksonService.toBytes(obj);
+    }
+
+    private String concatUrlWithParams(String url, String... params) {
+        UrlBuilder urlBuilder = templateUrlBuilder.duplicate();
+        urlBuilder.setPath(url);
+        if (params != null && params.length > 0) {
+            urlBuilder.setQueryParams(convertListToParams(params));
         }
-        return res.toString();
+        return urlBuilder.build();
     }
-
-    private String objectToString(Object obj) throws JsonProcessingException {
-        return obj == null ? "" : JacksonService.toJson(obj);
-    }
-
-    private static String encode(String value) {
-        return URLEncoder.encode(value, StandardCharsets.UTF_8);
-    }
-
 }
