71d1a46ff33e2eeffc1996320816566cf0c53634
moxiaoying <1159230165@qq.com>
2025-04-13 22:34:25 +0800

    fix: fix bk autorecovery request error
    Signed-off-by: moxiaoying <1159230165@qq.com>

--- bookkeeper-admin/src/main/java/io/github/protocol/bookkeeper/admin/jdk/RecoveryBookieReqData.java
+++ bookkeeper-admin/src/main/java/io/github/protocol/bookkeeper/admin/jdk/RecoveryBookieReqData.java
@@ -1,21 +1,20 @@
 package io.github.protocol.bookkeeper.admin.jdk;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
 import java.util.List;
 
+@Getter
 public class RecoveryBookieReqData {
+    @JsonProperty("bookie_src")
     private List<String> bookieSrc;
-    private boolean deleteCookie;
 
-    public List<String> getBookieSrc() {
-        return bookieSrc;
-    }
+    @JsonProperty("delete_cookie")
+    private boolean deleteCookie;
 
     public void setBookieSrc(List<String> bookieSrc) {
         this.bookieSrc = bookieSrc;
-    }
-
-    public boolean isDeleteCookie() {
-        return deleteCookie;
     }
 
     public void setDeleteCookie(boolean deleteCookie) {
